a98b180a17a4d987888f7263b60f0dc16b39b367
suaveforge-deploy <actions@users.noreply.github.com>
2026-09-23 14:47:46 +0000

    deploy nuguradar 10459b0911af

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.NUGU_CONFIG={"apiBase":"https://api-nuguradar.suaveforge.com:18042","build":"Updated 2026.09.23 · build 56e0f079ebf7-c2676e1717f2-012f68ab0bb8"};
+window.NUGU_CONFIG={"apiBase":"https://api-nuguradar.suaveforge.com:18042","build":"Updated 2026.09.23 · build 56e0f079ebf7-c2676e1717f2-456e64ec2c6c"};
